6b4e7136e6e05a14a50abc7c919ab0635b3d2ddf
Alex Pop <al3xpop@gmail.com>
2014-11-06 14:36:51 +0000

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: myiis-cookbook
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "*** Hello from the myiis-cookbook::default recipe!"
