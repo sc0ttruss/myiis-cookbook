75fbb145a29efa38c3957cf1196e7ee1f0c4062e
Alex Pop <al3xpop@gmail.com>
2015-07-13 16:18:23 +0100

    added timezone recipe and attr

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,5 @@
+default['myiis-cookbook']['timezone']="UTC"
+
 default['myiis-cookbook']['chrome']['url'] = 'https://s3-eu-west-1.amazonaws.com/apop-bucket/GoogleChrome.msi'
 # checksum obtained by running on a MAC: shasum -a 256 GoogleChromeStandaloneEnterprise.msi
 default['myiis-cookbook']['chrome']['checksum'] = '465b6d4ec0cc855a96471a9739195e7a2effebc28bbf60a7bd4738c3556dec14'
